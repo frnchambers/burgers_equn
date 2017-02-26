36b926c7f6fa61ef351adc3c085f63667f514541
frnchambers <frnchambers@gmail.com>
2017-02-26 22:42:13 +0100

    minor changes to lagrange, updated grid to work for any vector type

--- src/Grid/Grid.hpp
+++ src/Grid/Grid.hpp
@@ -6,105 +6,94 @@
 #include <cstdlib>
 #include <array>
 #include <cmath>
-#include <string>
 
 
+namespace grid_types {
+  template <size_t N>
+  using vector = std::array<N,double>;
+  //template <size_t N>
+  using matrix = boost::numeric::ublas::matrix< double > mat_type;
+}
+
+
+template <typename vec_type>
+void norm_vec (vec_type &v, double norm=1.0) {  // normalise vector so that largest value is norm
+  double v_biggest=0.0;
+  for ( const auto & vi : v )
+    if ( std::abs(vi) > std::abs(v_biggest) )
+      v_biggest = vi;
+  for ( double &vi : vpts )
+    vi *= norm / v_biggest;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 // ---------------------------------------------------------------------------------------------- //
 template <size_t N>  // ------------------------------------------------------------------------- //
-class Grid {  // ------------------------------------------------- Base Class to define a grid -- //
+class Grid_base {  // ------------------------------------------------- Base Class to define a grid -- //
 protected:
-  std::array<N,double> vec_type xpts, wpts;
-  const double a = -1.0, x0 = 0.0, b = 1.0;  // end points and 'center' of the grid
-////// to be defined and depend on the grid
-  virtual double x_func(const size_t & i) const = 0;
- public:  // ------------------------------------------------------------------------------------ //
+  grid_types::vector<N> vec xpts, wpts;
+  const double a = -1.0, x0 = 0.0, b = 1.0;  // end points and 'center' of the grid 
+  virtual double x_func(const size_t & i) const = 0;  // depends on the grid
+  Grid_base ( double a_in, double x0_in, double b_in )
+    : a(a_in), x0(x0_in), b(b_in)
+  {}
+  void auto_grid() {
+    for (size_t i = 0; i < N; ++i)
+      xpts[i] = x_func(i);
+    w_from_x();
+  }
+  void w_from_x () {  // for grids that don't have a simple analytic w
+    for ( size_t i=0; i < N; ++i ) {
+      w[i] = 1.0;
+      for ( size_t k=0; k < N; ++k ) {
+        if (k != i)
+          w[i] *= x[i]-x[k];
+      }
+      w[i] = 1.0/w[i];
+    }
+    norm_vec(w);
+  }
+public:  // ------------------------------------------------------------------------------------- //
 ////// return size of grid
   size_t size() const {
     return N;
   }
 ////// call element i of x or w grid
-  double &operator[] (const size_t & i) {
-    return xpts[i];
-  }
-  double &w (const size_t & i) {
-    return wpts[i];
-  }
-protected:  // -- initialise the grid ----------------------------------------------------------- //
-  Grid ( double a_in, double x0_in, double b_in )
-    : a(a_in), x0(x0_in), b(b_in)
-  {}
+  double &operator[] (const size_t & i) { return xpts[i]; }
+  double &w (const size_t & i) { return wpts[i]; }
 }; // ------------------------------------------------------------------------------------------- //
-
-void auto_grid() {  // initialise x_grid and weights from this grid
-  for (size_t i = 0; i < N; ++i)
-    xpts[i] = x_func(i);
-  w_from_x();
-}
-template <typename vec_type>
-void w_from_x (const vec_type &x, vec_type &w) {  // for grids that don't have a simple analytic w
-  for (size_t i = 0; i < N; i++) {
-    wpts[i] = 1.0;
-    for (size_t k = 0; k < N; k++)
-      if (k != i)
-        wpts[i] *= xpts[i]-xpts[k];
-    wpts[i] = 1.0/wpts[i];
-  }
-  scale_vec(w);
-}
-template <typename vec_type>
-void scale_vec ( vec_type &w ) {  // scale vector so that largest value is 1
-  double w_big = 0.0;
-  for ( const double &w : wpts )
-    if ( std::abs(w) > w_big )
-      w_big = std::abs(w);
-  for ( double &w : wpts )
-    w /= w_big;
-}
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-
+////////////////////////////////////////////////////////////////////////////////////////////////////
 // ---------------------------------------------------------------------------------------------- //
-class Even : public Grid {  // -- Evenly space grid --------------------------------------------- //
- public:
-  Even(size_t N_in,
-        double a_in = -1.0, double x0_in = 0.0, double b_in = 1.0)
-    : Grid(N_in, "Even", a_in, x0_in, b_in) {
-    auto_grid();
-  }
-
-  void reset(double a_in, double x0_in, double b_in) {
-    a = a_in; x0 = x0_in; b = b_in;
-    auto_grid();
-  }
-
+template <size_t N>  // ------------------------------------------------------------------------- //
+class Even : public Grid_base<N> {  // ------------------------------------------ Evenly space grid -- //
  private:
 ////// default parameters for grid centered at 0, with boundaries: -1,1
   double x_func(const size_t & i) const {
-    return a + ( b-a ) * static_cast<double>(i) / static_cast<double>(N_points-1);
+    return a + ( b-a ) * static_cast<double>(i) / static_cast<double>(N-1);
   }
+ public:
+  Even ( double a_in = -1.0, double x0_in = 0.0, double b_in = 1.0)
+    : Grid_base<N>::Grid_base(a_in, x0_in, b_in)
+  {
+    auto_grid();
+  }
+  // void reset(double a_in, double x0_in, double b_in) {
+  //   a = a_in; x0 = x0_in; b = b_in;
+  //   Grid_base<N>::auto_grid();
+  // }
 };  // ------------------------------------------------------------------------------------------ //
 
 
-
+////////////////////////////////////////////////////////////////////////////////////////////////////
 // ---------------------------------------------------------------------------------------------- //
-class Sinh : public Grid {  // -- Shiny log - A.L. Watts 2016 ----------------------------------- //
- public:
-  Sinh(size_t N_in,
-        double a_in = -1.0, double x0_in = 0.0, double b_in = 1.0)
-    : Grid(N_in, "Sinh", a_in, x0_in, b_in) {
-    set_params(a, x0, b);
-    auto_grid();
-  }
-
-  void reset(double a_in, double x0_in, double b_in) {
-    a = a_in; x0 = x0_in; b = b_in;
-    set_params(a, x0, b);
-    auto_grid();
-  }
-
+template <size_t N>  // ------------------------------------------------------------------------- //
+class Sinh : public Grid_base<N> {  // ------------------------------- Shiny log -> A.L. Watts 2016 -- //
  private:
 ////// default parameters for grid centered at 0, with boundaries: -1,1
-  double d = -1.0, m = 2.0;
+  const double d=-1.0, m=2.0;
   //  formulae for x0, m, and d: x = x0 + sinh( d + m * i/(N-1) )
   //  for x 'element of' [a,b]:
   //                -> d = asinh( a-x0 )
@@ -114,80 +103,94 @@ class Sinh : public Grid {  // -- Shiny log - A.L. Watts 2016 ------------------
     return x0 + std::sinh( z );
   }
 ////// set up parameters that characterise the grid
-  void set_params(const double & a_in, const double & x0_in, const double & b_in) {
-    d = std::asinh(a_in - x0_in);
-    m = std::asinh(b_in - x0_in) - d;
+  double d_param (const double & a_in, const double & x0_in, const double & b_in) {
+    return std::asinh(a_in - x0_in);
   }
+  double m_param (const double & a_in, const double & x0_in, const double & b_in) {
+    return std::asinh(b_in - x0_in) - std::asinh(a_in - x0_in);
+  }
+public:
+  Sinh( double a_in = -1.0, double x0_in = 0.0, double b_in = 1.0)
+    : Grid_base<N>::Grid_base(a_in, x0_in, b_in),
+      d(d_param(a,b,c)), m(m_param(a,b,c))
+  {
+    Grid_base<N>::auto_grid();
+  }
+  // void reset(double a_in, double x0_in, double b_in) {
+  //   a = a_in; x0 = x0_in; b = b_in;
+  //   m=m_param(a, x0, b);
+  //   d=d_param(a, x0, b);
+  //   auto_grid();
+  // }
 };  // ------------------------------------------------------------------------------------------ //
 
 
 
 
 // ---------------------------------------------------------------------------------------------- //
-class Cheb_1 : public Grid {  // -- gauss-lobato mesh ------------------------------------------- //
- public:
-  Cheb_1(size_t N_in,
-         double a_in = -1.0, double x0_in = 0.0, double b_in = 1.0)
-      : Grid(N_in, "Cheb_1", a_in, 0.5*(a_in+b_in), b_in) {
-    set_grid();
-  }
-  void reset(double a_in, double b_in) {
-    a = a_in; b = b_in;
-    set_grid();
-  }
-
- private:
+template <size_t N>  // ------------------------------------------------------------------------- //
+class Cheb_1 : public Grid_base<N> {  // ---------------------------------------- gauss-lobato mesh -- //
+private:
   double x_func(const size_t & i) const {
     double t = std::cos(M_PI * static_cast<double>(2*i + 1)
-                        / static_cast<double>(2*N_points) );
+                        / static_cast<double>(2*N) );
     return 0.5*(a+b) - 0.5*(a-b)*t;
   }
   double w_func(const size_t & i) const {
     return ( i%2 == 1 ? -1.0 : 1.0 ) *
         std::sin(M_PI * static_cast<double>(2*i + 1)
-                 / static_cast<double>(2*N_points) );
+                 / static_cast<double>(2*N) );
   }
   void set_grid() {
-    for ( size_t i = 0; i < N_points; ++i ) {
+    for ( size_t i = 0; i < N; ++i ) {
       xpts[i] = x_func(i);
       wpts[i] = w_func(i);
     }
   }
+ public:
+  Cheb_1(double a_in = -1.0, double b_in = 1.0)
+    : Grid_base<N>::Grid_base(a_in, 0.5*(a_in+b_in), b_in)
+  {
+    set_grid();
+  }
+  // void reset(double a_in, double b_in) {
+  //   a = a_in; b = b_in;
+  //   set_grid();
+  // }
 };  // ------------------------------------------------------------------------------------------ //
 
 
 
 // ---------------------------------------------------------------------------------------------- //
-class Cheb_2 : public Grid {  // ---------------------------------------------------------------- //
- public:
-  Cheb_2(size_t N_in,
-         double a_in = -1.0, double x0_in = 0.0, double b_in = 1.0)
-    : Grid(N_in, "Cheb_2", a_in, 0.5*(a_in+b_in), b_in) {
-    a = a_in; b = b_in;
-    set_grid();
-  }
-  void reset(double a_in, double x0_in, double b_in) {
-    x0_in=x0_in;
-    a = a_in; b = b_in;
-    set_grid();
-  }
-
+template <size_t N>  // ------------------------------------------------------------------------- //
+class Cheb_2 : public Grid_base<N> {  // -------------------------------------------------------- //
  private:
   double x_func(const size_t & i) const {
     double t = std::cos(M_PI * static_cast<double>(i)
-                        / (static_cast<double>(N_points-1) ) );
+                        / (static_cast<double>(N-1) ) );
     return 0.5*(a+b) + 0.5*(a-b)*t;
   }
   double w_func(const size_t & i) const {
-    return ( i == 0 || i == N_points-1 ? 0.5 : 1.0 )
+    return ( i == 0 || i == N-1 ? 0.5 : 1.0 )
         * (i%2 == 1 ? -1.0 : 1.0);
   }
   void set_grid() {
-    for (size_t i = 0; i < N_points; ++i) {
+    for (size_t i = 0; i < N; ++i) {
       xpts[i] = x_func(i);
       wpts[i] = w_func(i);
     }
   }
+ public:
+  Cheb_2(double a_in = -1.0, double b_in = 1.0)
+    : Grid_base<N>::Grid_base(a_in, 0.5*(a_in+b_in), b_in)
+  {
+    set_grid();
+  }
+  // void reset(double a_in, double x0_in, double b_in) {
+  //   x0_in=x0_in;
+  //   a = a_in; b = b_in;
+  //   set_grid();
+  // }
 };  // ------------------------------------------------------------------------------------------ //
 
 
--- src/Interpolation/Interp_lagrange.hpp
+++ src/Interpolation/Interp_lagrange.hpp
@@ -13,62 +13,44 @@ namespace lagrange_types {
   typedef boost::numeric::ublas::matrix< double > mat_type;
 }
 
-template <class grid_type>
+template <class Grid_type, size_t N>
 class lagrange { // ----------------------------------------------------------------------------- //
   // Spectral Methods routines given in:
   //          Berrut & Trefethen 2004 "Barycentric Lagrange Interpolation"
   //                             - SIAM Review Vol. 46, No. 3, pp. 501-517
 public:
+//private:
 
-  typedef lagrange_types::vec_type vec_type;
-  typedef lagrange_types::mat_type mat_type;
-
-  //private:
-
-////// datapoints for interpolation
-  size_t N_points;  
 ////// grid structure
-  grid_type xpts;
+  Grid_type &grid;
 ////// matrices used to calculate derivatives at gridpoints
-  mat_type
+  grid_types::matrix
     d1lpts,  // first derivative
     d2lpts;  // second derivative
 
-  const std::string name="lagrange";
-
 public:
 
-  lagrange ( size_t N_in )
-    : N_points( N_in ), xpts(N_in),
-      d1lpts( N_in,N_in,0.0 ), d2lpts( N_in,N_in,0.0 )
+  lagrange ( Grid &grid_in )
+    : xpts(grid_in), 
   {
     update_derivs();
   }
 
-  lagrange ( grid_type & xpts_in )
-    : N_points( xpts_in.size() ), xpts(xpts_in),
-      d1lpts( xpts_in.size(),xpts_in.size(),0.0 ), d2lpts( xpts_in.size(),xpts_in.size(),0.0 )
+  lagrange ()
+    : grid(),
+      d1lpts( N,N,0.0 ), d2lpts( N,N,0.0 )
   {
     update_derivs();
   }
 
-  size_t size () {
-    return N_points;
+  size_t size () const {
+    return N;
   }
   double operator[] ( const size_t & i ) const {
     return xpts[i];
   }
-  double x ( const size_t & i ) const {
-    return xpts[i];
-  }
-  std::string diff_name () const {
-    return name;
-  }
-  std::string grid_name () const {
-    return xpts.name;
-  }
 
-  ////// interpolate, at points x, arbitrary function on grid of interpolation points
+////// interpolate, at points x, arbitrary function on grid of interpolation points
   double operator() ( const vec_type & fpts, const double & x ) const {
     return interpolate( fpts, x );
   }
@@ -93,7 +75,7 @@ public:
   }
 
 
-  ////// print grid, wpts and derivative matrices
+////// print grid, wpts and derivative matrices
   void print_data () const {
 
     std::cout << "points:\n";
@@ -130,87 +112,74 @@ public:
 
 private:
 
-  ////// interpolate, at points x, arbitrary function on grid of interpolation points
-  // using formula (4.2) - the big one in the box
+////// interpolate, at points x, arbitrary function on grid of interpolation points
+// using formula (4.2) - the big one in the box
   double interpolate ( const vec_type & fpts, const double x ) const {
     double numer=0, denom=0;
-
     for( size_t j=0; j<N_points; j++ ) {
       numer += xpts.w(j) / ( x - xpts[j] ) * fpts[j];
       denom += xpts.w(j) / ( x - xpts[j] );
     }
-
     return numer / denom;
   }
 
+// --------------------------------------------------------------------------------------- //
+// --------- update the matrix d1lpts and d2lpts for derivatives at points xpts ---------- //
+// -------- equations (9.4) and (9.5), TYPO IN PAPER!!! use formulae given below --------- //
+// --------------------------------------------------------------------------------------- //
 
-  // --------------------------------------------------------------------------------------- //
-  // --------- update the matrix d1lpts and d2lpts for derivatives at points xpts ---------- //
-  // -------- equations (9.4) and (9.5), TYPO IN PAPER!!! use formulae given below --------- //
-  // --------------------------------------------------------------------------------------- //
-
-  ////// first derivative
-  // D^{(1)}_{ij} =   l_j'(x_i)
-  //                            =   \frac{ w_j/w_i }{ x_i-x_j }  ... i \neq j
-  //                            = - \sum_{k \neq j} l_j'(x_k)    ... i  = j
+////// first derivative
+// D^{(1)}_{ij} =   l_j'(x_i)
+//                            =   \frac{ w_j/w_i }{ x_i-x_j }  ... i \neq j
+//                            = - \sum_{k \neq j} l_j'(x_k)    ... i  = j
   void update_first_deriv () {
 
-    //// off diagonal components, formula (9.4)
+  // off diagonal components, formula (9.4)
     for ( size_t i=0; i<N_points; i++ )
       for ( size_t j=0; j<N_points; j++ )
         if ( j!=i )
           d1lpts(i,j) = ( xpts.w(j)/xpts.w(i) ) / ( xpts[i]-xpts[j] );
 
-    //// diagonal components, formula (9.5)
-    for ( size_t j=0; j<N_points; j++ ) {
-      for ( size_t i=0; i<N_points; i++ ) {
-        if ( i!=j ) {
+  // diagonal components, formula (9.5)
+    for ( size_t j=0; j<N_points; j++ )
+      for ( size_t i=0; i<N_points; i++ )
+        if ( i!=j )
           d1lpts(i,i) -= d1lpts(i,j);
-        }
-      }
-    }
+
   } // end update_first_deriv ()
 
 
-  ////// second derivative
-  // D^{(2)}_{ij} =    l_j''(x_i)
-  //                            = -2 l_j'(x_i) \left( \frac{1}{x_i-x_j} + \sum_{k \neq i} l_k'(x_i) \right)  ... i \neq j
-  //                            = -  \sum_{k \neq j} l_j''(x_k)  ... i = j
+////// second derivative
+// D^{(2)}_{ij} =    l_j''(x_i)
+//                            = -2 l_j'(x_i) \left( \frac{1}{x_i-x_j}
+//                              + \sum_{k \neq i} l_k'(x_i) \right)  ... i \neq j
+//                            = -  \sum_{k \neq j} l_j''(x_k)  ... i = j
   void update_second_deriv () {
 
-    //// off diagonal components, formula (9.4)
-    for ( size_t i=0; i<N_points; i++ ) {
-      for ( size_t j=0; j<N_points; j++ ) {
+  // off diagonal components, formula (9.4)
+    for ( size_t i=0; i<N_points; i++ )
+      for ( size_t j=0; j<N_points; j++ )
         if ( i!=j ) {
 
           double sum=0.0;
-          for ( size_t k=0; k<N_points; k++ ) {
+          for ( size_t k=0; k<N_points; k++ )
             if ( k!=i ) {
               sum += d1lpts(i,k);
-            }
-          }
 
           d2lpts(i,j) = -2.0 * d1lpts(i,j) * ( sum + ( 1.0/( xpts[i]-xpts[j] ) ) );
         }
-      }
-    }
 
-    //// diagonal components, formula (9.5)
-    for ( size_t j=0; j<N_points; j++ ) {
-      for ( size_t i=0; i<N_points; i++ ) {
-        if ( i!=j ) {
+  // diagonal components, formula (9.5)
+    for ( size_t j=0; j<N_points; j++ )
+      for ( size_t i=0; i<N_points; i++ )
+        if ( i!=j )
           d2lpts(j,j) -= d2lpts(j,i);
-        }
-      }
-    }
 
   } // end update_deriv ()
 
 
 }; // --------------------------------------------------------------------------------------------------------- //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
 
 
 
